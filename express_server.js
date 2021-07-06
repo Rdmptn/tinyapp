166146c8680ed9f242968ec4fc7f93384c61bdd1
Cameron <cameron_uniac@hotmail.com>
2021-07-06 14:58:56 -0400

    Added logic for edit button and associated POST (update) request

--- express_server.js
+++ express_server.js
@@ -62,6 +62,11 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect("/urls");
 });
 
+app.post("/urls/:shortURL", (req, res) => {
+  urlDatabase[req.params.shortURL] = `http://www.${req.body.newURL}`;
+  res.redirect("/urls");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
